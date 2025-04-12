52202cf92ff4de04120440432e2e407bfa5a21d5
weigui404 <1424289641@qq.com>
2025-04-12 17:35:44 +0800

    li

--- custom/doc.go
+++ custom/doc.go
@@ -1,2 +1,9 @@
 // Package custom 注册用户自定义插件于此
 package custom
+
+import (
+	_ "github.com/FloatTech/ZeroBot-Plugin/custom/plugin/antirecall"        // 反撤回
+	_ "github.com/FloatTech/ZeroBot-Plugin/custom/plugin/deepseek"        // deepseek
+	_ "github.com/FloatTech/ZeroBot-Plugin/custom/plugin/delreply"        // 回复撤回
+	_ "github.com/FloatTech/ZeroBot-Plugin/custom/plugin/moehu"        // 图片
+)
--- main.go
+++ main.go
@@ -63,14 +63,14 @@ import (
 	//                               vvvv                               //
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/custom"                   // 自定义插件合集
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ahsai"             // ahsai tts
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/ahsai"             // ahsai tts
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aifalse"           // 服务器监控
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"            // 随机老婆
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/alipayvoice"       // 支付宝到账语音
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/animetrace"        // AnimeTrace 动画/Galgame识别
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/autowithdraw"      // 触发者撤回时也自动撤回
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/baiduaudit"        // 百度内容审核
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/base16384"         // base16384加解密
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/base16384"         // base16384加解密
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/base64gua"         // base64卦加解密
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/baseamasiro"       // base天城文加解密
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibili"          // b站相关
@@ -109,7 +109,7 @@ import (
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/magicprompt"       // magicprompt吟唱提示
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/mcfish"            // 钓鱼模拟器
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/midicreate"        // 简易midi音乐制作
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/minecraftobserver" // Minecraft服务器监控&订阅
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/minecraftobserver" // Minecraft服务器监控&订阅
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/moyu"              // 摸鱼
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/moyucalendar"      // 摸鱼人日历
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/music"             // 点歌
@@ -145,7 +145,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wife"              // 抽老婆
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordcount"         // 聊天热词
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordle"            // 猜单词
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ygo"               // 游戏王相关插件
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/ygo"               // 游戏王相关插件
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/ymgal"             // 月幕galgame
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/yujn"              // 遇见API
 
